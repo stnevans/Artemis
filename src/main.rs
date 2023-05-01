5d1b021fe1569688418d0f2f5c7497fcf4406a2e
Stuart Nevans Locke <mc12stoud@gmail.com>
2023-05-01 02:21:19 -0400

    Clean up some code

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,6 @@ mod bb_utils;
 
 
 fn main() {
-    println!("Hello, world!");
-    // demo_eval();
-    // demo_search();
-    // test_board(&board_from_fen("rnbqkbnr/pppp1ppp/8/4p3/5PP1/8/PPPPP2P/RNBQKBNR b KQkq - 0 1"));
     uci::uci_loop();
 }
 
